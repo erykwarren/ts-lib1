43e0607abfa34d2f8f4bae4267a8f1c681862365
Eryk Warren <emw@mwarren.com>
2016-01-09 17:50:29 -0500

    fix backticks

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import * as os from 'os';
 
 export class Foo1 {
   talk(): string {
-    return 'I am Foo1. This hostname is ${os.hostname()}.';
+    return `I am Foo1. This hostname is ${os.hostname()}.`;
   }
 }
